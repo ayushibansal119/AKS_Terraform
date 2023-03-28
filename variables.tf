d34c6455212b2a22a33d7bd80f561c1ce04986f9
ayushib456 <94466531+ayushib456@users.noreply.github.com>
2023-03-28 16:15:04 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,15 @@
+# variable "shriram_dr_name" {
+#   type = string
+# }
+
+variable "prod_rg_location" {
+  type = string
+}
+
+variable "Environment" {
+  type = string
+}
+
+# variable "shriram_prod_location" {
+#   type = string
+# }
